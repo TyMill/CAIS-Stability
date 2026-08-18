3fa700f65b2ae1f1cb9efbbc5c9cc10fddb844b1
Tymoteusz Miller <81069997+TyMill@users.noreply.github.com>
2026-08-18 19:36:51 +0200

    feat: add package API

---
+++ src/cais_stability/__init__.py
@@ -0,0 +1,5 @@
+"""CAIS-Stability public API."""
+
+from .core import DynamicGovernance, GovernanceConfig, GovernanceMode, SystemState
+
+__all__ = ["DynamicGovernance", "GovernanceConfig", "GovernanceMode", "SystemState"]
